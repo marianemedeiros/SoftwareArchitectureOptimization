c99ef7799f736fbcc24c4aeb32bfd708a1f85620
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-05-12 00:00:36 -0300

    correções na metrica de arquitetura em camadas.

--- src/metrics/style/MetricLayerArch.java
+++ src/metrics/style/MetricLayerArch.java
@@ -17,19 +17,20 @@ public class MetricLayerArch {
 	 * 	Rule 1. layer n can't make a communication with layer (n-2)
 	 * */
 	public Penalty verifyStyle(Integer class1, Integer class2, Integer layer1, Integer layer2, Penalty penalty){
-			if(layer2 > (layer1+1)){
+			if(layer2 > (layer1+1) || layer1 > (layer2+1)){
 				if(!penalty.classBreak.containsKey(class1)){
 					penalty.classBreak.put(class1, 1);
 				}else{
 					penalty.classBreak.put(class1, penalty.classBreak.get(class1)+1);
 				}
+				if(!penalty.classBreak.containsKey(class2)){
+					penalty.classBreak.put(class2, 1);
+				}else{
+					penalty.classBreak.put(class1, penalty.classBreak.get(class2)+1);
+				}
 				Integer[] r = new Integer[2];
 				r[0] = class1; r[1] = class2;
 				penalty.listBadRel.add(r);
-			}else{
-				if(!penalty.classBreak.containsKey(class1)){
-					penalty.classBreak.put(class1, 1);
-				}
 			}
 		return penalty;
 	}
