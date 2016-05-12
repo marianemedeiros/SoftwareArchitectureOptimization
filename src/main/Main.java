16a84d9bfc84411c0c5934f50f04189fae66c369
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-05-12 17:28:47 -0300

    atualização.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,9 @@ import aco.entities.Architecture;
 public class Main {
 	// constants
 	public static boolean SHOW_LOGS = false; 
-	public static int ITERATIONS = 50;
-	public static int ANTS = 100;
-	public static double RO = 0.7; //used in uptadePheromone() at class Colony.
+	public static int ITERATIONS = 20;
+	public static int ANTS = 10;
+	public static double RO = 0.2; //used in uptadePheromone() at class Colony.
 	public static double ALPA = 0.9; //used in calculatesProbability() at class Probability. 
 	public static double BETA = 0.0;  //used in calculatesProbability() at class Probability. (heuristic information)
 	
@@ -36,13 +36,13 @@ public class Main {
 		
 
 		//Architecture architecture = new Architecture(pathSS, "modelSS"); //arquitetura dos exemplos sem estilo
-		//Architecture architecture = new Architecture(path, "modelCS"); //arquitetura dos exemplos com estilo
+		Architecture architecture = new Architecture(path, "modelCS"); //arquitetura dos exemplos com estilo
 		
 		//Architecture architecture = new Architecture(apacheAntStyle,ant11Style);// arquitetura do apache-ant com estilo em camadas
 		//Architecture architecture = new Architecture(apacheAnt,ant11);// arquitetura do apache-ant sem estilo
 		
 		//Architecture architecture = new Architecture(apacheAntStyle,ant11Style);// arquitetura do apache-ant com estilo em camadas
-		Architecture architecture = new Architecture(apacheAnt,ant13);// arquitetura do apache-ant sem estilo
+		//Architecture architecture = new Architecture(apacheAnt,ant13);// arquitetura do apache-ant sem estilo
 
 		architecture.initAntSystem();
 	}
