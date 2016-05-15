1731f683e71cc0db77b42dda10461c8983e3e256
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-05-15 00:31:18 -0300

    inserindo funções para adicionar elementos nas listas.

--- src/aco/entities/Penalty.java
+++ src/aco/entities/Penalty.java
@@ -14,5 +14,18 @@ public class Penalty {
 	
 	public ArrayList<Integer[]> listBadRel = new ArrayList<Integer[]>(); // relações que quebram a regra
 	public HashMap<Integer,Integer> classBreak = new HashMap<Integer, Integer>(); // quantidade de vezes que uma classe quebra a regra.
-
+	public double heuristicInformation = 0.0;
+	
+	public void addToClassBreak(Integer idClass){
+		if(!classBreak.containsKey(idClass)){
+			classBreak.put(idClass, 1);
+		}else{
+			classBreak.put(idClass, classBreak.get(idClass)+1);
+		}
+	}
+	
+	public void addToListBasRel(Integer[] i){
+		listBadRel.add(i);
+	}
+	
 }
