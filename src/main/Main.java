672d90111330bf3398aa5bd10044e607013ac0bb
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-05-24 02:31:42 -0300

    alterações para ler parametros de entrada de um arquivo.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,23 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import loadModel.Solution;
 import aco.entities.Architecture;
 import aco.entities.Parametro;
 
 public class Main {
 	public static boolean SHOW_LOGS = false; 
+	public final static int trials = 10;
 
+	/*
 	public static String path = "/modelos_UML/Estilo_em_Camada/ModeloCamada";
 	public static String pathSS = "/modelos_UML/Estilo_em_Camada/ModeloCamadaSemStyle";
 	public static String path1 = "/modelos_UML/Estilo_ClienteServidor/ModeloClienteServidor3";
@@ -15,7 +25,7 @@ public class Main {
 
 	public static String apacheAnt = "/modelos_UML";
 	public static String apacheAntStyle = "/modelos_UML/ApacheAnt11Style";
-	public static String apacheAnt13Style = "/modelo_UML/ApacheAnt13Style";
+	public static String apacheAnt13Style = "/modelos_UML/ApacheAnt13Style";
 
 	public static String ant11 = "apache-ant11";
 	public static String ant16 = "apache-ant16";
@@ -25,51 +35,140 @@ public class Main {
 
 	public static String ant11Style = "apache-ant11Style";
 	public static String ant13Style = "apache-ant13Style";
+	*/
 
+	public static String entrada = "entrada";
+	public static double[] alphas;
+	public static double[] betas;
+	public static double[] evaporationRates;
+	public static int[] iterations;
+	public static int[] ants;
+	
+	public static String nameFile;
+	public static String pathFile;
+	
 	public static void main(String[] args) throws Exception {
 		File file = new java.io.File("");   //Dummy file
 		String  abspath = file.getAbsolutePath();
 		String[] split = abspath.split("/");
-		
+
 		String home = "/" + split[1] +"/"+ split[2];
-		File directory = new File(home + "/resultadosTeste/");
+		File directory = new File(home + "/resultados/");
 		directory.mkdir();
 		String results = directory.getAbsolutePath();
-		
+
+		extractDatas(abspath);
 		/*
 		double[] alphas = {0.4, 0.5, 0.6, 0.7, 0.8, 1.0};
 		double[] betas = {0.8, 0.2, 0.4, 0.5, 0.6, 0.7, 0.8, 1.0};
 		double[] evaporationRates = {0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
 		int[] iterations = {20, 40, 80, 160, 320, 400};
 		int[] ants = {100, 40, 80, 160, 320}; 
-		*/
-		
-		double[] alphas = {0.6};
-		double[] betas = {0.0,0.3};
-		double[] evaporationRates = {0.4};
-		int[] iterations = {100};
-		int[] ants = {15};
-		
-		int trials = 10;
+		 */
 
+		Architecture architecture = null;
+		Solution s = null;
 		for (int ant : ants) {
 			for (int iteration : iterations) {
 				for (double alpha : alphas) {
 					for (double beta : betas) {
 						for (double ro :evaporationRates) {
-							//for (int i = 0; i < trials; i++) {
-								Parametro param = new Parametro(iteration, ant, ro, alpha, beta);
-								System.err.println(iteration + ", " + ant + ", " + ro + ", " + alpha + "," + beta);
-								//Architecture architecture = new Architecture(abspath+apacheAntStyle,ant11Style,param,results);// arquitetura do apache-ant sem estilo
-								//architecture.initAntSystem(0);
-
-								Architecture architecture = new Architecture(abspath+apacheAntStyle,ant11Style,param,results);//arquitetura do apache-ant sem estilo
-								architecture.initAntSystem(0);
-							//}
+							Parametro param = new Parametro(iteration, ant, ro, alpha, beta);
+							architecture = new Architecture(Main.pathFile,Main.nameFile,param,results);// arquitetura do apache-ant sem estilo
+							for (int i = 0; i < trials; i++) {
+								s = architecture.initAntSystem(i);
+							}
+							saveMediaInfoRelationClasses(s, results, Main.nameFile + "_" + "modeloOtimizado" + "_" +
+									iteration + "_" + ant + "_" + ro + "_" + 
+									alpha + "_" + beta + "_infos_relationXclasses_medias"+".csv", architecture.evolutionClassRelationI,
+									architecture.evolutionClassRelationE);
+							saveMediaInfoComponentsClasses(s, results, Main.nameFile + "_" + "modeloOtimizado" + "_" +
+									iteration + "_" + ant + "_" + ro + "_" + 
+									alpha + "_" + beta + "_infos_componentXclasses_medias"+".csv", architecture.evolutionComponentClass);
 						}
 					}
 				}
 			}
 		}
 	}
+
+	private static void saveMediaInfoRelationClasses(Solution s, String dir, String nameFile, int[] v1, int[] v2) throws IOException{
+		File file = new File(dir,nameFile);
+		FileWriter fileWriter = new FileWriter(file,true);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+		bufferedWriter.write("Class,Internal,External");
+		bufferedWriter.newLine();
+
+		for (Entry<Integer, Integer> class_ : s.classComponent.entrySet()) {
+			bufferedWriter.write(class_.getKey() + "," + (v1[class_.getKey()]/(double)Main.trials) + "," + (v2[class_.getKey()]/(double)Main.trials));
+			bufferedWriter.newLine();
+		}
+		bufferedWriter.close();
+	}
+
+	private static void saveMediaInfoComponentsClasses(Solution s, String dir, String nameFile, int[] v1) throws IOException{
+		File file = new File(dir,nameFile);
+		FileWriter fileWriter = new FileWriter(file,true);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+		bufferedWriter.write("Comp,Class");
+		bufferedWriter.newLine();
+
+		for (Entry<Integer, Set<Integer>> comp_ : s.componentClasses.entrySet()) {
+			bufferedWriter.write(comp_.getKey() + "," + (v1[comp_.getKey()]/(double)Main.trials));
+			bufferedWriter.newLine();
+		}
+		bufferedWriter.close();
+	}
+	
+	private static void extractDatas(String abspath) throws IOException{
+		System.err.println();
+		File file = new File(abspath,Main.entrada);
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+		
+		String line = bufferedReader.readLine();
+		while(line != null){
+			if(line.contains("abspath")){
+				String[] s = line.replace(" ", "").split("=");
+				Main.pathFile = s[1];
+			}else if(line.contains("name")){
+				Main.nameFile = line.replace(" ", "").split("=")[1];
+			}else if(line.contains("alphas")){
+				String[] v = line.replace(" ", "").split("=")[1].split(",");
+				Main.alphas = new double[v.length];
+				for (int i = 0; i < v.length; i++) {
+					Main.alphas[i] = Double.valueOf(v[i]);
+				}
+			}else if(line.contains("betas")){
+				String[] v = line.replace(" ", "").split("=")[1].split(",");
+				Main.betas = new double[v.length];
+				for (int i = 0; i < v.length; i++) {
+					Main.betas[i] = Double.valueOf(v[i]);
+				}
+			}else if(line.contains("evaporationRates")){
+				String[] v = line.replace(" ", "").split("=")[1].split(",");
+				Main.evaporationRates = new double[v.length];
+				for (int i = 0; i < v.length; i++) {
+					Main.evaporationRates[i] = Double.valueOf(v[i]);
+				}
+			}else if(line.contains("iterations")){
+				String[] v = line.replace(" ", "").split("=")[1].split(",");
+				Main.iterations = new int[v.length];
+				for (int i = 0; i < v.length; i++) {
+					Main.iterations[i] = Integer.valueOf(v[i]);
+				}
+			}else if(line.contains("ants")){
+				String[] v = line.replace(" ", "").split("=")[1].split(",");
+				Main.ants = new int[v.length];
+				for (int i = 0; i < v.length; i++) {
+					Main.ants[i] = Integer.valueOf(v[i]);
+				}
+			}
+			line = bufferedReader.readLine();
+		}
+	}
+	
+
 }
+
