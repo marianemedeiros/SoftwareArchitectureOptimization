c17fea7ed51ad47f3b9ca036c5e3ad64ea4058ab
mariane medeiros <marianeaffonsomedeiros@gmail.com>
2016-05-29 22:21:35 -0300

    arrumando incrementado de classe que quebra a regra.

--- src/metrics/style/MetricClientServerArch.java
+++ src/metrics/style/MetricClientServerArch.java
@@ -20,11 +20,20 @@ public class MetricClientServerArch {
 			if(!penalty.classBreak.containsKey(class1)){
 				penalty.classBreak.put(class1, 1);
 			}else{
-				Integer sum = penalty.classBreak.get(class1)+1;
-				penalty.classBreak.put(class1, sum);
+				penalty.classBreak.put(class1, penalty.classBreak.get(class1)+1);
 			}
-//			System.out.println("---- " + class1 +":"+ type1 + " ---" +class2 + ":"+ type2);
-//			System.out.println("QUEBROU REGRA: " + class1 + ": " + penalty.classBreak.get(class1));
+			
+			if(!penalty.classBreak.containsKey(class2)){
+				penalty.classBreak.put(class2, 1);
+			}else{
+				penalty.classBreak.put(class2, penalty.classBreak.get(class2)+1);
+			}
+
+			
+			
+			//System.err.println("---- " + class1 +":"+ type1 + " ---" +class2 + ":"+ type2);
+			//System.err.println("QUEBROU REGRA: " + class1 + ": " + penalty.classBreak.get(class1));
+			//System.err.println("QUEBROU REGRA: " + class2 + ": " + penalty.classBreak.get(class2));
 
 			Integer r[] = new Integer[2];
 			r[0] = class1; r[1] = class2;
@@ -32,6 +41,8 @@ public class MetricClientServerArch {
 		}
 		return penalty;
 	}
+	
+
 }
 
 
